3aebb12d5f7bfd41b3709192e73b86a829d69165
Alizey <alizeycheema@gmail.com>
2025-02-06 10:25:59 +0400

    adding login and signup form

--- nijat/urls.py
+++ nijat/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.conf import settings
 
 urlpatterns = [
-    path('' , views.index , name='index')
+    path('' , views.index , name='index'),
+    path('signup' , views.signup , name='signup'),
+    path('login' , views.login, name='login')
 ]
--- nijat/views.py
+++ nijat/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request , 'nijat/index.html')
+
+def signup(request):
+    return render(request , 'nijat/signup.html')
+
+def login(request):
+    return render(request , 'nijat/login.html')
